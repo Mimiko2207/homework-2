f3dd194c13dcb9352d0f90fb20bceeed0a916df6
Mimiko2207 <loparev.m@mail.ru>
2025-06-21 16:46:28 +0700

    ДЗ-2

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,70 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    System.out.println("ЗАДАЧА 1");
+    var dog = 8.0;
+    var cat = 3.6;
+    var paper = 763789;
+    System.out.println(dog);
+    System.out.println(cat);
+    System.out.println(paper);
+        System.out.println("ЗАДАЧА 2");
+        dog = dog + 4;
+        System.out.println(dog);
+        cat = cat + 4;
+        System.out.println(cat);
+        paper = paper + 4;
+        System.out.println(paper);
+            System.out.println("ЗАДАЧА 3");
+            dog = dog - 3.5;
+            System.out.println(dog);
+            cat = cat - 1.6;
+            System.out.println(cat);
+            paper = paper - 7639;
+            System.out.println(paper);
+                System.out.println("ЗАДАЧА 4");
+                var friend = 19;
+                System.out.println(friend);
+                friend = (friend + 2);
+                System.out.println(friend);
+                friend = friend / 7;
+                System.out.println(friend);
+                    System.out.println("ЗАДАЧА 5");
+                    var frog = 3.5;
+                    System.out.println(frog);
+                    frog = frog * 10;
+                    System.out.println(frog);
+                    frog = frog / 3.5;
+                    System.out.println(frog);
+                    frog = frog + 4;
+                    System.out.println(frog);
+                        System.out.println("ЗАДАЧА 6");
+                        var boxer1 = 78.2;
+                        System.out.println(boxer1);
+                        var boxer2 = 82.7;
+                        System.out.println(boxer2);
+                        var differenceWeight = (boxer1 - boxer2);
+                        System.out.println(differenceWeight);
+                            System.out.println("ЗАДАЧА 7");
+                            var weight = (boxer2 % boxer1);
+                            System.out.println(weight);
+                                System.out.println("ЗАДАЧА 8");
+                                var time = 640;
+                                var timeOneMan = 8;
+                                System.out.println(time);
+                                System.out.println(timeOneMan);
+                                var people = time / timeOneMan;
+                                System.out.println(people);
+                                System.out.println("Всего работников в компании " + people + " человек");
+                                var employee = 94;
+                                var timeOneEmpoyee = time / employee;
+                                System.out.println("Если в компании работет " + employee + " человек, то всего " + timeOneEmpoyee +
+                                        " часов работы может быть поделено между сотрудниками");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+
+
+
+
+
     }
 }
